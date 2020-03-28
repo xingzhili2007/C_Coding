ef02bcb49b8da85920c448ee62363d797ce8070e
李行之 <xingzhili2007@163.com>
2020-03-28 20:27:37 +0800

    Initial Commit

---
+++ C_Coding/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C_Coding
+//
+//  Created by 李行之 on 2020/3/28.
+//  Copyright © 2020 李行之. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
